ce0b7e52429290b5a7e29f312d99b0732bab9d52
puppy0cam <puppy0caminminecraft@gmail.com>
2019-08-04 21:36:34 +1000

    remove keys from ssl agent

--- request/HttpsAgent.js
+++ request/HttpsAgent.js
@@ -1,10 +1,5 @@
-import { readFileSync } from "fs";
 import { Agent } from "https";
-export const cert = readFileSync(new URL("./YOURPUBLIC.pem", import.meta.url));
-export const key = readFileSync(new URL("./YOURPRIVATE.key", import.meta.url));
 const HttpsAgent = new Agent({
-    cert,
-    key,
     keepAlive: true,
     keepAliveMsecs: 10000,
     maxFreeSockets: 25,
