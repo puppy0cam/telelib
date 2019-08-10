0c2d509aec34ba66d69ada3b0f773ef7230b59ae
puppy0cam <puppy0caminminecraft@gmail.com>
2019-08-10 14:55:50 +1000

    create proper String function

--- String.js
+++ String.js
@@ -1,1 +1,10 @@
-export default String;
+export default (value) => {
+    "use strict";
+    if (typeof value === "string") {
+        return value;
+    } else if (value instanceof String) {
+        return value.valueOf();
+    } else {
+        throw new TypeError("This value is not a string!");
+    }
+};
